e05e8719400975c0084cd0da7cefb45016230792
georgep9 <g-patt@outlook.com>
2022-01-25 10:04:02 +1000

    refactored secrets manager signer
    Signed-off-by: georgep9 <g-patt@outlook.com>

--- core/src/main/java/tech/pegasys/web3signer/core/AwsSecretsManagerSigner.java
+++ core/src/main/java/tech/pegasys/web3signer/core/AwsSecretsManagerSigner.java
@@ -2,23 +2,22 @@ package tech.pegasys.web3signer.core;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
-import tech.pegasys.signers.secp256k1.EthPublicKeyUtils;
 import tech.pegasys.teku.bls.BLS;
 import tech.pegasys.teku.bls.BLSSecretKey;
 import tech.pegasys.web3signer.core.signing.ArtifactSignature;
 import tech.pegasys.web3signer.core.signing.ArtifactSigner;
 import tech.pegasys.web3signer.core.signing.BlsArtifactSignature;
 
-import java.security.interfaces.ECPublicKey;
-
 public class AwsSecretsManagerSigner implements ArtifactSigner {
 
-  private AwsSecretsManagerKey awsSecretsManagerKey;
   private String publicKey;
+  private byte[] privateKey;
 
   public AwsSecretsManagerSigner(String secretName){
-    this.awsSecretsManagerKey = new AwsSecretsManagerKey(secretName);
-    this.publicKey = this.awsSecretsManagerKey.getPublicKey();
+    AwsSecretsManagerKey awsSecretsManagerKey = new AwsSecretsManagerKey(secretName);
+    this.publicKey = awsSecretsManagerKey.getPublicKey();
+    this.privateKey = awsSecretsManagerKey.getPrivateKey();
+    awsSecretsManagerKey.close();
   }
 
   @Override
@@ -28,7 +27,7 @@ public class AwsSecretsManagerSigner implements ArtifactSigner {
 
   @Override
   public ArtifactSignature sign(Bytes message) {
-    BLSSecretKey blsSecretKey = BLSSecretKey.fromBytes(Bytes32.wrap(this.awsSecretsManagerKey.getPrivateKey()));
+    BLSSecretKey blsSecretKey = BLSSecretKey.fromBytes(Bytes32.wrap(this.privateKey));
     return new BlsArtifactSignature(BLS.sign(blsSecretKey, message));
   }
 }
